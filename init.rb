b81729f97a9fff7930e99783206463f9e8545fce
Damian Baćkowski <dbackowski@artcom.pl>
2011-08-29 15:25:21 +0200

    First commit

---
+++ init.rb
@@ -0,0 +1,3 @@
+# Include hook code here
+
+require 'error_messages_for_helper'
---
+++ lib/error_messages_for_helper.rb
@@ -0,0 +1,22 @@
+module ErrorMessagesForHelper
+  def error_messages_for(object_name)
+    model = instance_variable_get("@#{object_name}")
+    
+    unless model.nil? || model.errors.empty?  
+      errors_list = capture do
+        content_tag :ul do 
+          model.errors.each do |attr,message|
+            concat(content_tag :li, message)
+          end
+        end
+      end
+      
+      result = content_tag :h2, t(:header_message, :scope => 'error_messages_for', :default => 'Unable to save data.').html_safe 
+      result << content_tag(:p, t(:error_list, :scope => 'error_messages_for', :default => 'Errors list:').html_safe)
+      result << errors_list
+      result = content_tag :div, result, :id => 'errorExplanation', :class => 'errorExplanation'
+    end
+  end
+end
+
+ActionView::Base.send :include, ErrorMessagesForHelper
