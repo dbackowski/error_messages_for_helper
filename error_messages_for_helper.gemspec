8f6e598fdb13443028394331acfdb9024fb3b5dd
Damian Baćkowski <dbackowski@artcom.pl>
2012-09-21 12:01:04 +0200

    Added html_options to ErrorMessagesForHelper module, updated README.

--- error_messages_for_helper.gemspec
+++ error_messages_for_helper.gemspec
@@ -2,18 +2,18 @@
 
 Gem::Specification.new do |gem|
   gem.name    = 'error_messages_for_helper'
-  gem.version = '1.0.1'
+  gem.version = '1.1.0'
   gem.date    = Time.now.strftime('%Y-%m-%d')
-  
-  gem.summary = "Active Record error messages helper for Rails 3.0.x"
+
+  gem.summary = "Active Record error messages helper for Rails 3.2.x"
   gem.description = "error_messages_for_helper provides a simple helper to show ActiveRecord validation errors (just like old helper error_messages_for for Rails-2.3.x)."
-  
+
   gem.author  = 'Damian Baćkowski'
   gem.email    = 'damianbackowski@gmail.com'
   gem.homepage = 'https://github.com/paki-paki/error_messages_for_helper'
-  
+
   gem.rubyforge_project = nil
   gem.has_rdoc = false
-  
+
   gem.files = Dir['Rakefile', '{bin,lib,rails,test,spec}/**/*', 'README*', 'LICENSE*'] & `git ls-files -z`.split("\0")
 end
--- lib/error_messages_for_helper.rb
+++ lib/error_messages_for_helper.rb
@@ -1,20 +1,26 @@
 module ErrorMessagesForHelper
+  @@html_options = { :id => 'error_explanation' }
+
+  def self.html_options(options)
+    @@html_options = options
+  end
+
   def error_messages_for(object_name)
     model = instance_variable_get("@#{object_name}")
-    
-    unless model.nil? || model.errors.empty?  
+
+    if model.present? && model.errors.present?
       errors_list = capture do
-        content_tag :ul do 
-          model.errors.each do |attr,message|
+        content_tag :ul do
+          model.errors.each do |attr, message|
             concat(content_tag :li, message)
           end
         end
       end
-      
-      result = content_tag :h2, t(:header_message, :scope => 'error_messages_for', :default => 'Unable to save data.').html_safe 
+
+      result = content_tag :h2, t(:header_message, :scope => 'error_messages_for', :default => 'Unable to save data.').html_safe
       result << content_tag(:p, t(:error_list, :scope => 'error_messages_for', :default => 'Errors list:').html_safe)
       result << errors_list
-      result = content_tag :div, result, :id => 'error_explanation'
+      result = content_tag :div, result, @@html_options
     end
   end
 end
